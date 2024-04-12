0b3ff1e4d393d62cc0c0a6ba2f4979b2dec940fc
Artem Bahdasarian <bahdasarian.california@gmail.com>
2024-04-12 14:09:15 -0700

    new commit

---
+++ js/swiper.js
@@ -0,0 +1,18 @@
+"use strict"
+
+const Swiper = new Swiper('.b6-chp2-swiper',{
+
+    // general settings
+    direction: 'vertical',
+    loop: true,
+
+
+    // Prev Next
+
+    navigation:{
+        nextEl: '.b6-chp2-swiper-next',
+        prevEl: '.b6-chp2-swiper-prev',
+    },
+    
+
+})
